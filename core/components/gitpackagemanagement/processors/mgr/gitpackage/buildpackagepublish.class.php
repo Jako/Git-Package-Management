80fef9643809dd2106b59aae535dbc7df43b7943
Thomas Jakobi <thomas.jakobi@partout.info>
2016-09-03 12:38:39 +0200

    Autopublish with Packeteer

--- core/components/gitpackagemanagement/processors/mgr/gitpackage/buildpackagepublish.class.php
+++ core/components/gitpackagemanagement/processors/mgr/gitpackage/buildpackagepublish.class.php
@@ -2,23 +2,26 @@
 require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/model/gitpackagemanagement/gpc/gitpackageconfig.class.php';
 require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/model/gitpackagemanagement/builder/gitpackagebuilder.class.php';
 require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/processors/mgr/gitpackage/buildpackage.class.php';
+
 /**
  * Clone git repository and install it
  *
  * @package gitpackagemanagement
  * @subpackage processors
  */
-class GitPackageManagementBuildPackagePublishProcessor extends GitPackageManagementBuildPackageProcessor {
+class GitPackageManagementBuildPackagePublishProcessor extends GitPackageManagementBuildPackageProcessor
+{
 
-    public function process() {
-
+    public function process()
+    {
         $process = parent::process();
         if ($process['success'] !== true) {
             return $process;
         };
 
+        // @todo upload the package with sftp or similar
         $source = $this->config->getPackagePath() . '/_packages/' . $this->builder->getTPBuilder()->getSignature() . '.transport.zip';
-        $targetPath = realpath(MODX_BASE_PATH . 'extras/');
+        $targetPath = realpath(MODX_BASE_PATH . $this->modx->getOption('packeteer.site_extras_path'));
         $target = $targetPath . '/_packages/' . $this->builder->getTPBuilder()->getSignature() . '.transport.zip';
         copy($source, $target);
         chmod($target, 0666);
@@ -27,18 +30,42 @@ class GitPackageManagementBuildPackagePublishProcessor extends GitPackageManagem
         if (!file_exists($package_info)) {
             $info_file = fopen($package_info, 'w');
             fwrite($info_file, "<?php\n" .
-                "return array(
+                "return array(\n" .
                 "    'name' => '{$this->config->getLowCaseName()}',\n" .
                 "    'displayname' => '{$this->config->getName()}',\n" .
                 "    'description' => '{$this->config->getDescription()}',\n" .
                 "    'author' => '{$this->config->getAuthor()}',\n" .
-                "    'modx_version' => '2.3',\n"
+                "    'modx_version' => '2.3');\n"
             );
             fclose($info_file);
         }
         chmod($package_info, 0666);
 
-        return $this->success();
+        $packageName = $this->config->getLowCaseName();
+        $beta = (bool)preg_match('/.*?-(b|beta)\\d*/i', $this->builder->getTPBuilder()->getSignature());
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $this->modx->getOption('packeteer.site_assets_url') . 'components/packeteer/connector.php',
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => array(
+                'action' => 'web/packages/scan',
+                'package' => $packageName,
+                'beta' => $beta,
+                'hash' => hash('sha256', $this->modx->getOption('packeteer.site_id') . $packageName . ((string)$beta))
+            )
+        ));
+        $result = json_decode(curl_exec($curl), true);
+        $this->modx->log(xPDO::LOG_LEVEL_DEBUG, $result['message'], '', 'GitPackageManagementBuildPackagePublishProcessor');
+        curl_close($curl);
+
+        if (isset($result['success']) && $result['success'] == true) {
+            return $this->success($result['message']);
+        } else {
+            return $this->failure($result['message']);
+        }
     }
 }
+
 return 'GitPackageManagementBuildPackagePublishProcessor';
